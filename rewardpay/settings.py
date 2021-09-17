b0c1313aa0e54e700ebf859243cf4ffb3bdbc596
jack-jackhui <65952706+jack-jackhui@users.noreply.github.com>
2021-09-17 16:33:41 +1000

    updated settings.py

--- rewardpay/settings.py
+++ rewardpay/settings.py
@@ -17,7 +17,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SANDBOX_DIR = "C:/Users/jack_/sandbox"
+SANDBOX_DIR = "/data/sandbox"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,9 +31,9 @@ except KeyError:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
